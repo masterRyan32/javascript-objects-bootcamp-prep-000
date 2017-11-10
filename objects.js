da4702bd80b3934534c4d4d4efd3f3869d936e36
Ryan Shur <ryanshur32@yahoo.com>
2017-11-10 22:40:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,8 +7,7 @@ function  updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  var playlist = {artistName:"songTitle"}
-  Delete playlist.artistName
+  delete playlist.artistName
 
   return playlist
 }
